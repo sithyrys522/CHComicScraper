86b9a52b18ca5cf270916652251e34d7d3b837ff
Joey Mullica <joeymullica522@gmail.com>
2020-08-18 18:58:01 -0400

    Can call image pull from UI

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 var path = require('path')
+const scrape = require('./scrape');
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 })
 
 app.post('/', (req, res) => {
+    scrape.getComic(5639);
     res.send('You posted!')
 })
 
--- server/scrape.js
+++ server/scrape.js
@@ -2,33 +2,38 @@ var fs = require('fs'),
     request = require('request');
 const cheerio = require('cheerio');
 
-var urlToDownload;
+const getComic = comicID => {
 
-var download = function(uri, filename, callback){
-    request.head(uri, function(err, res, body){
-      console.log('content-type:', res.headers['content-type']);
-      console.log('content-length:', res.headers['content-length']);
-  
-      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    var urlToDownload;
+
+    var download = function(uri, filename, callback){
+        request.head(uri, function(err, res, body){
+          console.log('content-type:', res.headers['content-type']);
+          console.log('content-length:', res.headers['content-length']);
+        
+          request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+        });
+      };
+
+    request({
+        method: 'GET',
+        url: `http://explosm.net/comics/${comicID}/`
+    }, (err, res, body) => {
+
+        if (err) return console.error(err);
+
+        let $ = cheerio.load(body);
+
+        let comicURL = $('#main-comic').attr('src');
+
+        urlToDownload = `http:${comicURL}`;
+
+        download(urlToDownload, './images/comic.png', function(){
+            console.log('done');
+          });
+      
     });
-  };
 
-request({
-    method: 'GET',
-    url: 'http://explosm.net/comics/5639/'
-}, (err, res, body) => {
+    }
 
-    if (err) return console.error(err);
-
-    let $ = cheerio.load(body);
-
-    let comicURL = $('#main-comic').attr('src');
-
-    urlToDownload = `http:${comicURL}`;
-
-    download(urlToDownload, './images/comic.png', function(){
-        console.log('done');
-      });
-    
-});
-
+exports.getComic = getComic
