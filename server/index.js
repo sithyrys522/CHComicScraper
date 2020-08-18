7cb254486fddba25299949a7fc6d578b7039fd90
Joey Mullica <joeymullica522@gmail.com>
2020-08-18 19:28:19 -0400

    Can now enter specific comic number to download

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+//const router = require('router')
 const app = express()
 const port = 3000
 var path = require('path')
 const scrape = require('./scrape');
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 })
 
 app.post('/', (req, res) => {
-    scrape.getComic(5639);
+    scrape.getComic(req.body.comicNumber);
     res.send('You posted!')
 })
 
