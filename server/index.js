959d96c4a825fb337676c4890b0edd089f0bf627
Joey Mullica <joeymullica522@gmail.com>
2020-08-19 08:27:36 -0400

    Added functionalty to return to home screen after loading comic.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const port = 3000
 var path = require('path')
 const scrape = require('./scrape');
 
+app.use(express.static('public'));
+app.use(express.static('images'));
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -17,7 +20,7 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     scrape.getComic(req.body.comicNumber);
-    res.send('You posted!')
+    res.sendFile(path.join(__dirname, '../client/posted.html'));
 })
 
 app.get('/test.html', (req, res) => {
